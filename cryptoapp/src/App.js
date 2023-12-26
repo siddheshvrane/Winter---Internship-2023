d896570edd252c4d3aeafc71b41b11f8704e42dc
CryingBaker <atharvinfo1811@gmail.com>
2023-12-26 23:03:17 +0530

    Verified working of API on a sample page

--- cryptoapp/src/App.js
+++ cryptoapp/src/App.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import Coins from './components/Coins.js'
 
 function App() {
--- cryptoapp/src/components/Coins.js
+++ cryptoapp/src/components/Coins.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import CoinItem from './CoinItem.js'
 
-const Coins = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Coins = (props) => {
+    return (
+        <div className='container'>
+            <div className='heading'>
+                        <p>#</p>
+                        <p className='CoinName'>Coin</p>
+                        <p>Price</p>
+                        <p>24h</p>
+                        <p className='hide-mobile'>Volume</p>
+                        <p className='hide-mobile'>Market Cap</p>
+                </div>
+
+        {props.coins.map(coins => {
+                return (
+                    <CoinItem coins={coins} />
+                )
+        })}
+        </div>
+    )
 }
-
 export default Coins
